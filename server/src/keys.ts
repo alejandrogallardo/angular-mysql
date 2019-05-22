08c165c86778eccc4c2cd11238b8f2ee7e2d0e37
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2019-05-22 17:07:29 -0600

    Se elimino el password de la base de datos

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,7 +3,7 @@ export default {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '#velMax.MyS84',
+        password: 'tu password',
         database: 'ng_games_db'
         // database: 'talleruno'
     }
